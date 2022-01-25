e1c1f2d7cee6c1846e32d06ad8df53eee2475c99
Chamarti Sreemannarayana <93900514+Sreemannarayana15@users.noreply.github.com>
2022-01-25 10:47:52 +0530

    Update biggerarray.c

--- biggerarray.c
+++ biggerarray.c
@@ -2,16 +2,22 @@
 int main(){
     // 10 rollnumbers , their subject names and marks
     int student[10][4] = {{1,4,5,68},{34,4,3,90},{14,2,3,99},{128,1,2,99},{1,4,5,91},{1,4,5,68},{34,4,3,90},{14,2,3,99},{128,1,2,99},{1,4,5,91}};
-    // for (int i=0;i<10;i++){
+    // for (int i=0;i<5;i++){
     //     for (int j=0;j<4;j++){
     //         printf("%d   ", student[i][j]);
     //     }
     //     printf("\n");
     // }
-    int row; 
-    printf("Enter the row : "); 
-    scanf("%d",&row);
-    for (int k=0;k<4;k++){
-        printf("%d ",student[row-1][k]);
+    // int row; 
+    // printf("Enter the row : "); 
+    // scanf("%d",&row);
+    // for (int k=0;k<4;k++){
+    //     printf("%d ",student[row-1][k]);
+    // }
+    int column;
+    printf("Enter the column number : ");
+    scanf("%d",&column);
+    for (int k=0;k<10;k++){
+        printf("%d\n",student[k][column-1]);
     }
 }
