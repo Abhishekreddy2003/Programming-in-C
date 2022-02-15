8ba14a27558f378b4eb02e2d7710b292bf9aaa30
Chamarti Sreemannarayana <93900514+Sreemannarayana15@users.noreply.github.com>
2022-02-15 09:12:23 +0530

    Add files via upload

---
+++ playingWithStuct.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+struct playingWithStuct
+{
+    char name[20];
+    float rating;
+    int neg;
+};
+int main()
+{
+    struct playingWithStuct p1, p2;
+    printf("Enter Name : "); scanf("%s",p1.name);
+    printf("Enter negative values : "); scanf("%d",p1.neg);
+    printf("Enter Rating : "); scanf("%f",p1.rating);
+    p2=p1;
+    printf("%s",p2.name);
+}
+// https://github.com/Sreemannarayana15/C_Programs.git
